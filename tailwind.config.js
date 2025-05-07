77067f26bee1352999b1a298c13b5ffc81890417
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-05-07 13:55:26 -0300

    fix layout.tsx and apply 'font-sans' class to body to integrate with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
